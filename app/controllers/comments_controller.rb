41796f0aa618962fe06ded2aa09da2e2c21ce8f8
gabe <gabrieljappleton@gmail.com>
2016-10-29 21:21:09 -0400

    basic authentication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,8 @@
 class CommentsController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret",
+except: [:index, :show]
+
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
